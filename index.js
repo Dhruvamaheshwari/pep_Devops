6309d0cc9b0d0e4365b63d6fe4a2728876772b4d
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-03-25 21:56:08 +0530

    file change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Dhruva");
 });
 app.post("/data", (req, res) => {
   const data = req.body;
